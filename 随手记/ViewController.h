a9bc0d46561b0539d50000e4618880d0dd179605
yorke <zc_and_zc@aliyun.com>
2013-12-30 22:21:54 +0800

    增加框架

---
+++ 随手记/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  随手记
+//
+//  Created by OPSOFT on 13-12-30.
+//  Copyright (c) 2013年 OPSOFT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
